2cbc85389f3923a9e03bec011a71e49fb3ce20e5
908115501 <908115501@qq.com>
2019-12-13 16:00:59 +0800

    author info

--- src/components/postDetail.vue
+++ src/components/postDetail.vue
@@ -57,7 +57,8 @@
           </div>
           <div v-for="(item,i) in comments"
                :key="i"
-               class="author-title reply-father">
+               class="author
+               -title reply-father">
             <el-avatar class="header-img"
                        :size="40"
                        :src="item.headImg"></el-avatar>
@@ -177,79 +178,79 @@ export default {
       to: '',
       toId: -1,
       comments: [
-        {
-          name: 'Lana Del Rey',
-          id: 19870621,
-          headImg: 'https://ae01.alicdn.com/kf/Hd60a3f7c06fd47ae85624badd32ce54dv.jpg',
-          comment: '我发布一张新专辑Norman Fucking Rockwell,大家快来听啊',
-          time: '2019年9月16日 18:43',
-          commentNum: 2,
-          like: 15,
-          inputShow: false,
-          reply: [
-            {
-              from: 'Taylor Swift',
-              fromId: 19891221,
-              fromHeadImg: 'https://ae01.alicdn.com/kf/H94c78935ffa64e7e977544d19ecebf06L.jpg',
-              to: 'Lana Del Rey',
-              toId: 19870621,
-              comment: '我很喜欢你的新专辑！！',
-              time: '2019年9月16日 18:43',
-              commentNum: 1,
-              like: 15,
-              inputShow: false
-            },
-            {
-              from: 'Ariana Grande',
-              fromId: 1123,
-              fromHeadImg: 'https://ae01.alicdn.com/kf/Hf6c0b4a7428b4edf866a9fbab75568e6U.jpg',
-              to: 'Lana Del Rey',
-              toId: 19870621,
-              comment: '别忘记宣传我们的合作单曲啊',
-              time: '2019年9月16日 18:43',
-              commentNum: 0,
-              like: 5,
-              inputShow: false
+        // {
+        //   name: 'Lana Del Rey',
+        //   id: 19870621,
+        //   headImg: 'https://ae01.alicdn.com/kf/Hd60a3f7c06fd47ae85624badd32ce54dv.jpg',
+        //   comment: '我发布一张新专辑Norman Fucking Rockwell,大家快来听啊',
+        //   time: '2019年9月16日 18:43',
+        //   commentNum: 2,
+        //   like: 15,
+        //   inputShow: false,
+        //   reply: [
+        //     {
+        //       from: 'Taylor Swift',
+        //       fromId: 19891221,
+        //       fromHeadImg: 'https://ae01.alicdn.com/kf/H94c78935ffa64e7e977544d19ecebf06L.jpg',
+        //       to: 'Lana Del Rey',
+        //       toId: 19870621,
+        //       comment: '我很喜欢你的新专辑！！',
+        //       time: '2019年9月16日 18:43',
+        //       commentNum: 1,
+        //       like: 15,
+        //       inputShow: false
+        //     },
+        //     {
+        //       from: 'Ariana Grande',
+        //       fromId: 1123,
+        //       fromHeadImg: 'https://ae01.alicdn.com/kf/Hf6c0b4a7428b4edf866a9fbab75568e6U.jpg',
+        //       to: 'Lana Del Rey',
+        //       toId: 19870621,
+        //       comment: '别忘记宣传我们的合作单曲啊',
+        //       time: '2019年9月16日 18:43',
+        //       commentNum: 0,
+        //       like: 5,
+        //       inputShow: false
 
-            }
-          ]
-        },
-        {
-          name: 'Taylor Swift',
-          id: 19891221,
-          headImg: 'https://ae01.alicdn.com/kf/H94c78935ffa64e7e977544d19ecebf06L.jpg',
-          comment: '我发行了我的新专辑Lover',
-          time: '2019年9月16日 18:43',
-          commentNum: 1,
-          like: 5,
-          inputShow: false,
-          reply: [
-            {
-              from: 'Lana Del Rey',
-              fromId: 19870621,
-              fromHeadImg: 'https://ae01.alicdn.com/kf/Hd60a3f7c06fd47ae85624badd32ce54dv.jpg',
-              to: 'Taylor Swift',
-              toId: 19891221,
-              comment: '新专辑和speak now 一样棒！',
-              time: '2019年9月16日 18:43',
-              commentNum: 25,
-              like: 5,
-              inputShow: false
+        //     }
+        //   ]
+        // },
+        // {
+        //   name: 'Taylor Swift',
+        //   id: 19891221,
+        //   headImg: 'https://ae01.alicdn.com/kf/H94c78935ffa64e7e977544d19ecebf06L.jpg',
+        //   comment: '我发行了我的新专辑Lover',
+        //   time: '2019年9月16日 18:43',
+        //   commentNum: 1,
+        //   like: 5,
+        //   inputShow: false,
+        //   reply: [
+        //     {
+        //       from: 'Lana Del Rey',
+        //       fromId: 19870621,
+        //       fromHeadImg: 'https://ae01.alicdn.com/kf/Hd60a3f7c06fd47ae85624badd32ce54dv.jpg',
+        //       to: 'Taylor Swift',
+        //       toId: 19891221,
+        //       comment: '新专辑和speak now 一样棒！',
+        //       time: '2019年9月16日 18:43',
+        //       commentNum: 25,
+        //       like: 5,
+        //       inputShow: false
 
-            }
-          ]
-        },
-        {
-          name: 'Norman Fucking Rockwell',
-          id: 20190830,
-          headImg: 'https://ae01.alicdn.com/kf/Hdd856ae4c81545d2b51fa0c209f7aa28Z.jpg',
-          comment: 'Plz buy Norman Fucking Rockwell on everywhere',
-          time: '2019年9月16日 18:43',
-          commentNum: 0,
-          like: 5,
-          inputShow: false,
-          reply: []
-        }
+        //     }
+        //   ]
+        // },
+        // {
+        //   name: 'Norman Fucking Rockwell',
+        //   id: 20190830,
+        //   headImg: 'https://ae01.alicdn.com/kf/Hdd856ae4c81545d2b51fa0c209f7aa28Z.jpg',
+        //   comment: 'Plz buy Norman Fucking Rockwell on everywhere',
+        //   time: '2019年9月16日 18:43',
+        //   commentNum: 0,
+        //   like: 5,
+        //   inputShow: false,
+        //   reply: []
+        // }
       ],
       postId: '',
       postData: {},
@@ -269,8 +270,7 @@ export default {
         likeNum: 100,
         avatar: 'https://ae01.alicdn.com/kf/Hd60a3f7c06fd47ae85624badd32ce54dv.jpg'
 
-      },
-      postLikeNum: 0
+      }
     }
   },
   directives: { clickoutside },
