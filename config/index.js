2bbf5268e32f303c044d00be86c1b4661dabdf42
TayloreZhou <824126263@qq.com>
2019-12-14 20:40:29 +0800

    refine 20:40

--- config/index.js
+++ config/index.js
@@ -18,6 +18,13 @@ module.exports = {
           '^/api': '/api'
         }
       },
+      '/server': {
+        target: 'http://localhost:8070',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/server': '/'
+        }
+      },
       '/boot': {
         target: 'http://localhost:8084',
         changeOrigin: true,
